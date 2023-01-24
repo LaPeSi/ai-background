fbd3a2992a6160fb621ef27f3d53cd4beca3b307
Pepe Simon <ppipotsch@gmail.com>
2023-01-24 13:55:40 +0100

    windows-default-combined

--- windows-11/update-ai-background.bat
+++ windows-11/update-ai-background.bat
@@ -1,5 +1,5 @@
 :loop
-curl -o "C:\Users\%USERNAME%\Downloads\ai-background.jpg" http://lape.si:8080/apps/ai-background/image
+curl -o "C:\Users\%USERNAME%\Downloads\ai-background.jpg" http://lape.si:8080/apps/ai-background/combined
 reg add "HKCU\Control Panel\Desktop" /v WallPaper /f /t REG_SZ /d "C:\Users\%USERNAME%\Downloads\ai-background.jpg"
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 timeout /t 30
